d502c22dd8f2a6f29644a5e335803921136fb7fe
Ayush <coding.carrots@gmail.com>
2020-06-24 22:35:24 +0530

    student api additions

--- senServer/settings.py
+++ senServer/settings.py
@@ -121,7 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static'
+STATIC_ROOT = './static'
 
 CORS_ORIGIN_ALLOW_ALL=True
 
