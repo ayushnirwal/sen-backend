4a9af5ee8baf666151b6fabf00fca67521ed52c0
Ayush <coding.carrots@gmail.com>
2020-06-24 23:33:07 +0530

    student api additions

--- senServer/urls.py
+++ senServer/urls.py
@@ -15,12 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from django.conf import settings
-from django.conf.urls.static import static
+from senServer import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('prof/', include("prof.urls")),
     path('stu/', include("stu.urls")),
     
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
