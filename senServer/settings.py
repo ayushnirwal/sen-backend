23c085bf33f0831c0426a8f988fc9856df8edbe2
Ayush <coding.carrots@gmail.com>
2020-06-25 00:59:30 +0530

    student api additions

--- senServer/settings.py
+++ senServer/settings.py
@@ -106,9 +106,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Internationalization
-# https://docs.djangoproject.com/en/3.0/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
+# https://docs.djangop
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage''
 
 TIME_ZONE = 'UTC'
 
